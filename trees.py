f67a68e6360eecfe95cf421bbf7722506067d8a4
Azurelyu <1029538915@qq.com>
2018-12-21 22:21:15 +0800

    Initial commit

---
+++ trees.py
@@ -0,0 +1,28 @@
+from math import log
+
+
+def calcShannonEnt(dataSet):
+    numEntries = len(dataSet)
+    labelCounts = {}
+    for featVec in dataSet:
+        currentLabel = featVec[-1]
+        if currentLabel not in labelCounts.keys():
+            labelCounts[currentLabel] = 0
+            labelCounts[currentLabel] += 1          # 到此为止是为所有可能的类创建字典。
+        shannonEnt = 0.0
+        for key in labelCounts:
+            prob = float(labelCounts[key])/numEntries
+            shannonEnt -= prob * log(prob,2)         # 以2为底求对数。
+        return shannonEnt
+
+
+
+
+def createDataSet():
+    dataSet = [[1, 1, 'yes'],
+               [1, 1, 'yes'],
+               [1, 0, 'no'],
+               [0, 1, 'no'],
+               [0, 1, 'no']]
+labels = ['no surfacing', 'flippers']
+return dataSet, labels
